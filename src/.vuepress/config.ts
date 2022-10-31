3704228ccf2c65f20364d65fa3f281b519546281
yszar <yszar@vip.qq.com>
2022-10-31 19:30:41 +0800

    feat: :sparkles: 修改博客配置
    修改博客配置，博客配置基本完成

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,18 +4,11 @@ import theme from "./theme.js";
 export default defineUserConfig({
   base: "/",
 
-  locales: {
-    "/en/": {
-      lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
-    },
-    "/": {
-      lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
-    },
-  },
+  lang: "zh-CN",
+
+  title: "编程学习网",
+  
+  description: "快来！一起来学编程！",
 
   theme,
 
